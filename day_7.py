71d04de177d98a473c75038700cc99d279a79fd4
Bhavanayerrawagari <159920071+Bhavanayerrawagari@users.noreply.github.com>
2024-03-21 13:06:07 +0530

    Add files via upload

---
+++ day_7.py
@@ -0,0 +1,30 @@
+'''
+set1={'python','java','data science'}
+set2={'ML','AI','R Language','python'}
+set3={'data analytics','robotics','deep learning'}
+-->wrong code          if set1 in set2 or set1 in set3:
+    print("joint")
+    break
+elif set2 in set1 or set2 in set3:
+    print("joint")
+    break
+elif set3 in set1 or set3 in set2:
+    print("joint")
+    break
+else:
+    print("disjoint")
+'''
+list1=["m","na","i","ke"]
+list2=["y","me","s","lly"]
+l3=[]
+i=0
+while i<len(list1):
+    l3.append(list1[i]+list2[i])
+    i+=1
+print(l3)
+def profile():
+    
+    
+  
+
+
